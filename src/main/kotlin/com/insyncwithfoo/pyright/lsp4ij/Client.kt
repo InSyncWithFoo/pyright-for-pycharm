a38f0701a06062c87d63b799ae3192cad963e753
InSyncWithFoo <insyncwithfoo@gmail.com>
2024-07-14 17:03:50 +0000

    Log settings on initialization

--- src/main/kotlin/com/insyncwithfoo/pyright/lsp4ij/Client.kt
+++ src/main/kotlin/com/insyncwithfoo/pyright/lsp4ij/Client.kt
@@ -1,6 +1,7 @@
 package com.insyncwithfoo.pyright.lsp4ij
 
 import com.insyncwithfoo.pyright.sdkPath
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.redhat.devtools.lsp4ij.ServerStatus
 import com.redhat.devtools.lsp4ij.client.LanguageClientImpl
@@ -16,12 +17,16 @@ private fun Project.createSettingsObject() = Settings().apply {
 internal class Client(project: Project) : LanguageClientImpl(project) {
     
     override fun createSettings() =
-        project.createSettingsObject()
+        project.createSettingsObject().also { LOGGER.info(it.toString()) }
     
     override fun handleServerStatusChanged(serverStatus: ServerStatus?) {
         if (serverStatus == ServerStatus.started) {
             triggerChangeConfiguration()
         }
+    }
+    
+    companion object {
+        val LOGGER = Logger.getInstance(Client::class.java)
     }
     
 }
