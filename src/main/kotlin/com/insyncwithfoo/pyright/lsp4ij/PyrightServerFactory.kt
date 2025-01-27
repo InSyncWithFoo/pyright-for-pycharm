a9f33eb31676955742c09e5c83e0c576e67f269a
InSync <insyncwithfoo@gmail.com>
2025-01-28 02:24:20 +0700

    Revert

--- src/main/kotlin/com/insyncwithfoo/pyright/lsp4ij/PyrightServerFactory.kt
+++ src/main/kotlin/com/insyncwithfoo/pyright/lsp4ij/PyrightServerFactory.kt
@@ -6,17 +6,12 @@ import com.insyncwithfoo.pyright.configurations.changePyrightOverrides
 import com.insyncwithfoo.pyright.configurations.models.add
 import com.insyncwithfoo.pyright.configurations.pyrightConfigurations
 import com.insyncwithfoo.pyright.configurations.pyrightExecutable
-import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.io.OSAgnosticPathUtil
-import com.intellij.openapi.vfs.VirtualFile
 import com.redhat.devtools.lsp4ij.LanguageServerEnablementSupport
 import com.redhat.devtools.lsp4ij.LanguageServerFactory
 import com.redhat.devtools.lsp4ij.client.LanguageClientImpl
-import com.redhat.devtools.lsp4ij.client.features.FileUriSupport
 import com.redhat.devtools.lsp4ij.client.features.LSPClientFeatures
 import com.redhat.devtools.lsp4ij.server.StreamConnectionProvider
-import java.net.URI
 
 
 internal const val SERVER_ID = "com.insyncwithfoo.pyright"
@@ -52,28 +47,10 @@ internal class PyrightServerFactory : LanguageServerFactory, LanguageServerEnabl
     }
     
     @Suppress("UnstableApiUsage")
-    override fun createClientFeatures(): LSPClientFeatures {
-        val features = object : LSPClientFeatures() {
-            override fun getFileUri(file: VirtualFile): URI {
-                val uri = FileUriSupport.DEFAULT.getFileUri(file).toString()
-                val prefix = "file:///"
-                
-                thisLogger().warn(uri)
-                thisLogger().warn(OSAgnosticPathUtil.startsWithWindowsDrive(uri.substring(prefix.length)).toString())
-                thisLogger().warn(prefix + uri[prefix.length].lowercase() + "%3A" + uri.substring(prefix.length + 2))
-                
-                if (uri.startsWith(prefix) && OSAgnosticPathUtil.startsWithWindowsDrive(uri.substring(prefix.length))) {
-                    return URI.create(prefix + uri[prefix.length].lowercase() + "%3A" + uri.substring(prefix.length + 2))
-                }
-                return URI.create(uri)
-            }
-        }
-        
-        return features.apply {
+    override fun createClientFeatures() = LSPClientFeatures().apply {
             hoverFeature = HoverFeature()
             diagnosticFeature = DiagnosticFeature()
             completionFeature = CompletionFeature()
         }
-    }
     
 }
