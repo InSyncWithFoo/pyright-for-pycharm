861f32a3db84016f658c354a6af5c3e8948d5a88
InSync <insyncwithfoo@gmail.com>
2025-01-27 09:47:28 +0700

    Log

--- src/main/kotlin/com/insyncwithfoo/pyright/lsp4ij/PyrightServerFactory.kt
+++ src/main/kotlin/com/insyncwithfoo/pyright/lsp4ij/PyrightServerFactory.kt
@@ -6,6 +6,7 @@ import com.insyncwithfoo.pyright.configurations.changePyrightOverrides
 import com.insyncwithfoo.pyright.configurations.models.add
 import com.insyncwithfoo.pyright.configurations.pyrightConfigurations
 import com.insyncwithfoo.pyright.configurations.pyrightExecutable
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.OSAgnosticPathUtil
 import com.intellij.openapi.vfs.VirtualFile
@@ -60,8 +61,12 @@ internal class PyrightServerFactory : LanguageServerFactory, LanguageServerEnabl
         
         override fun getFileUri(file: VirtualFile): URI {
             val uri = FileUriSupport.DEFAULT.getFileUri(file).toString()
-            
             val prefix = "file:///"
+            
+            thisLogger().warn(uri)
+            thisLogger().warn(OSAgnosticPathUtil.startsWithWindowsDrive(uri.substring(prefix.length)))
+            thisLogger().warn(prefix + uri[prefix.length].lowercase() + "%3A" + uri.substring(prefix.length + 2))
+            
             if (uri.startsWith(prefix) && OSAgnosticPathUtil.startsWithWindowsDrive(uri.substring(prefix.length))) {
                 return URI.create(prefix + uri[prefix.length].lowercase() + "%3A" + uri.substring(prefix.length + 2))
             }
